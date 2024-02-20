bf81f2aa4853e084666f8249f3936189a3f0e4ce
IshikaKhandelwal <IshikaKhandelwal02@gmail.com>
2024-02-19 17:04:33 -0800

    Fix linting issues

--- src/noisepy/seis/io/scedc_s3store.py
+++ src/noisepy/seis/io/scedc_s3store.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta, timezone
 from typing import Callable, List
-
 import obspy
 from datetimerange import DateTimeRange
 
