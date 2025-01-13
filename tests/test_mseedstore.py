a7682f7bef02a7bbf2ee620cbbb7f7385404f16c
Yiyu Ni <niyiyu@uw.edu>
2025-01-13 14:46:17 -0800

    feat: miniSEED store for local file (#44)
    * Add miniSEED store for io
    
    Signed-off-by: Yiyu Ni <niyiyu@uw.edu>
    
    * Fix pre-commit
    
    Signed-off-by: Yiyu Ni <niyiyu@uw.edu>
    
    * Add tests for mSEED store
    
    Signed-off-by: Yiyu Ni <niyiyu@uw.edu>
    
    * Correct pre-commit
    
    ---------
    
    Signed-off-by: Yiyu Ni <niyiyu@uw.edu>

---
+++ tests/test_mseedstore.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime, timezone
+
+from datetimerange import DateTimeRange
+
+from noisepy.seis.io.channelcatalog import XMLStationChannelCatalog
+from noisepy.seis.io.mseedstore import MiniSeedDataStore
+
+# timeframe for analysis
+start = datetime(2022, 1, 1, tzinfo=timezone.utc)
+end = datetime(2022, 1, 4, tzinfo=timezone.utc)
+timerange = DateTimeRange(start, end)
+
+DATA = os.path.join(os.path.dirname(__file__), "./data/mSEED")
+STATION_XML = os.path.join(os.path.dirname(__file__), "./data/CI/")
+catalog = XMLStationChannelCatalog(STATION_XML, path_format="{network}.{name}.xml")
+store = MiniSeedDataStore(DATA, catalog, date_range=timerange)
+
+
+def test_mseedstore():
+    span = store.get_timespans()
+    assert len(span) == 3
+
+    channels = store.get_channels(span[0])
+    assert len(channels) == 1
+    assert len(store.read_data(span[0], channels[0]).stream) == 1
