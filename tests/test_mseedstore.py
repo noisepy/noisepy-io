5c08229943bd01952beb25aabbed067a3a45bb3c
Yiyu Ni <niyiyu@uw.edu>
2025-01-13 14:37:45 -0800

    Correct pre-commit

--- tests/test_mseedstore.py
+++ tests/test_mseedstore.py
@@ -1,9 +1,10 @@
 import os
-
-from noisepy.seis.io.mseedstore import MiniSeedDataStore
-from noisepy.seis.io.channelcatalog import XMLStationChannelCatalog
 from datetime import datetime, timezone
+
 from datetimerange import DateTimeRange
+
+from noisepy.seis.io.channelcatalog import XMLStationChannelCatalog
+from noisepy.seis.io.mseedstore import MiniSeedDataStore
 
 # timeframe for analysis
 start = datetime(2022, 1, 1, tzinfo=timezone.utc)
@@ -12,8 +13,9 @@ timerange = DateTimeRange(start, end)
 
 DATA = os.path.join(os.path.dirname(__file__), "./data/mSEED")
 STATION_XML = os.path.join(os.path.dirname(__file__), "./data/CI/")
-catalog = XMLStationChannelCatalog(STATION_XML, path_format='{network}.{name}.xml')
+catalog = XMLStationChannelCatalog(STATION_XML, path_format="{network}.{name}.xml")
 store = MiniSeedDataStore(DATA, catalog, date_range=timerange)
+
 
 def test_mseedstore():
     span = store.get_timespans()
